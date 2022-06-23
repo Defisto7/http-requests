a02632c25d660c29e22bd85606b27a290c9ca3fd
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-06-23 11:18:39 +0300

    Master
    Using useEffect() For Requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,18 +8,19 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  
+  const fetchMoviesHandler = useCallback (async () => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch('https://swapi.dev/api/films/');
-
+      
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
-
+      
       const data = await response.json();
-
+      
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -33,8 +34,12 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
-
+  }, []);
+  
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  
   let content = <p>Found no movies.</p>
 
   if (movies.length > 0) {
